006aa82b491ba4fd76b8dac94f2400a62bb1c5da
caiiohenryk <caiohc.dev@gmail.com>
2026-03-28 15:46:42 -0300

    remembering stuff and doing some concatenation

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "I'm trying to memorize C++ syntax and learn a new language!" << std::endl;
+
+    // Now, I will try to use some C++ string concatenation with variables
+    int age = 21;
+    std::string name = "Caio";
+    std::cout << "Hello!" << " My name is " << name << " and I'm " << age << " yo" << std::endl;
+    std::cout << "Hope you like my code! :o" << std::endl;
+    return 0;
+}
